8388b7a955c3ad42fb37b1d77a48a9111261cd13
ottoa98@yahoo.com <ottoa98@yahoo.com>
2024-09-07 03:19:59 +0000

    Created family photo.jpg, army.jpg and family.jpeg; Edited ServerModule1 and Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -9,20 +9,22 @@ class Form1(Form1Template):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-    self.dom_nodes['submit'].addEventListener('click', self._handle_click)
+    self.dom_nodes['submit'].addEventListener('click', self._submit_click)
 
-  def _handle_click(self, event):
+  def _submit_click(self, event):
     # Prevent default form submission
     event.preventDefault()
 
-    name = self.dom_nodes['text'].value
+    name = self.dom_nodes['name'].value
+    email = self.dom_nodes['email'].value
     message = self.dom_nodes['message'].value
     # Call the server function to send an email
-    anvil.server.call('send_email', name, message)
+    #anvil.server.call('send_email', name, email, message)
 
-    self.dom_nodes['text'].value = ""
+    self.dom_nodes['name'].value = ""
+    self.dom_nodes['email'].value = ""
     self.dom_nodes['message'].value = ""
-    alert("Message sent successfully!")
+    anvil.alert(content="Message sent successfully!", title=f"Thank you, {name}", buttons=None)
     
 
 
--- server_code/ServerModule1.py
+++ server_code/ServerModule1.py
@@ -1,16 +1,14 @@
 import anvil.email
 import anvil.server
+from datetime import datetime
+from zoneinfo import ZoneInfo
 
 @anvil.server.callable
-def send_email(name, message):
-  # Send yourself an email each time feedback is submitted
-  anvil.email.send(to="ottoa98@gmail.com", 
-                   subject=f"Feedback from {name}",
-                   text=f"""
-                   
-  A new person has sent a message!
+def send_email(name, email, message):
+  utc_now = datetime.now().astimezone(ZoneInfo("UTC"))
+  eastern_time = utc_now.astimezone(ZoneInfo("America/New_York"))
 
-  Name: {name}
-  Feedback:
-  {message}
-  """)
+  anvil.email.send(from_name="Otto Portfolio Contact",
+                   to="ottoa98@gmail.com",
+                   subject=f"New Message from {name}: {eastern_time.strftime('%A %d %b %Y at %I:%M %p')}",
+                   text=f"{name} ({email}) sent you a message: {message}")
