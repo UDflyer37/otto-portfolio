067247abca7b312b51f8d0e0880706c649adb793
ottoa98@yahoo.com <ottoa98@yahoo.com>
2024-09-07 00:52:31 +0000

    Edited settings and Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -1,23 +1,26 @@
 from ._anvil_designer import Form1Template
-from anvil import *
-from anvil.js.window import 
-import anvil.js
+from anvil import 
+import anvil.server
+
 
 
 class Form1(Form1Template):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-    send=self.dom_nodes['send']
-    text=self.dom_nodes['text']
-    message=self.dom_nodes['message']
-    
     self.dom_nodes['send'].addEventListener('click', self._send_click)
 
-  def _send_click(self, **event_args):
-    self.raise_event('click')
-    print("this button was clicked")
+    
+  def submit_click(self, **event_args):
+      """Triggered when the submit button is clicked"""
+      name = self.text.text  # Get the value from the Name input field
+      message = self.message.text  # Get the value from the Message textarea
+
+      # Call the backend function to send the email
+      anvil.server.call('send_email', name, message)
+
+      # Optionally show a success message
+      alert(f"Thank you, {name}! Your message has been sent.")
+
     
       
-
-    # Any code you write here will run before the form opens.
